feba7387656b699846479b87eafab6ab306e94e9
THANAPHOL NANTAKASET(Sa-Tang) <satang.thevalue@gmail.com>
2024-03-06 09:50:41 +0700

    filter store_name
    filter col store_name "RUSH STOP / CHARLES CITY"

--- LAB 6_1_full.R
+++ LAB 6_1_full.R
@@ -1,2 +1,3 @@
 setwd("D:/OneDrive - Rajamangala University of Technology Thanyaburi/01 Learning/05 Year 66 Term 2/Financial Forecasting/Lab_R_iowa_liquor_sales(6-3-67)")
 df = read.csv(file = "LAB 6_1_full.csv")
+df_new <- filter(df,df$store_name == "RUSH STOP / CHARLES CITY")
