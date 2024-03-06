7a604c2a44ae5a2cbc99a7bee694851242a5257e
THANAPHOL NANTAKASET(Sa-Tang) <satang.thevalue@gmail.com>
2024-03-06 09:38:45 +0700

    read csv

---
+++ LAB 6_1_full.R
@@ -0,0 +1,2 @@
+setwd("D:/OneDrive - Rajamangala University of Technology Thanyaburi/01 Learning/05 Year 66 Term 2/Financial Forecasting/Lab_R_iowa_liquor_sales(6-3-67)")
+df = read.csv(file = "LAB 6_1_full.csv")
